687fd12e3e9e80f8b00dde9426be896bac711892
RO-HIT-MAURYA <40297562+RO-HIT-MAURYA@users.noreply.github.com>
2020-05-10 13:49:23 +0530

    View Pager 2 is implemented.

--- app/src/main/java/rohit/maurya/countrywisenews/activity/NewsDetailActivity.java
+++ app/src/main/java/rohit/maurya/countrywisenews/activity/NewsDetailActivity.java
@@ -3,8 +3,10 @@ package rohit.maurya.countrywisenews.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.PagerAdapter;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -32,44 +34,43 @@ public class NewsDetailActivity extends AppCompatActivity {
 
         JsonArray jsonArray = App.jsonArray;
         if (jsonArray != null)
-            activityNewsDetailBinding.verticalViewPager.setAdapter(new ViewPagerAdapter(jsonArray));
+            activityNewsDetailBinding.viewPager2.setAdapter(new ViewPagerAdapter2(jsonArray));
 
-        activityNewsDetailBinding.verticalViewPager.setCurrentItem(i);
+        activityNewsDetailBinding.viewPager2.setCurrentItem(i);
 
     }
 
-    private class ViewPagerAdapter extends PagerAdapter {
+    private class ViewPagerAdapter2 extends RecyclerView.Adapter<ViewPagerAdapter2.InnerClass>
+    {
         private JsonArray jsonArray = new JsonArray();
 
-        private ViewPagerAdapter(JsonArray jsonArray) {
+        private ViewPagerAdapter2(JsonArray jsonArray)
+        {
             this.jsonArray = jsonArray;
-        }
-
-        @Override
-        public int getCount() {
-            return jsonArray.size();
-        }
-
-        @Override
-        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-            return view.equals(object);
         }
 
         @NonNull
         @Override
-        public Object instantiateItem(@NonNull ViewGroup container, int position) {
-            //return super.instantiateItem(container, position);
-            View view = getLayoutInflater().inflate(R.layout.view_pager_item, container, false);
+        public ViewPagerAdapter2.InnerClass onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+        {
+            View view = getLayoutInflater().inflate(R.layout.view_pager_item,parent,false);
+
+            return new InnerClass(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewPagerAdapter2.InnerClass holder, int position)
+        {
             JsonObject jsonObject = (JsonObject) jsonArray.get(position);
 
             String string = jsonObject.get("urlToImage") + "";
             string = string.replace("\"", "");
             Log.e("imageIs", string);
             if (!string.isEmpty())
-                Picasso.get().load(string).into((ImageView) view.findViewById(R.id.imageView));
+                Picasso.get().load(string).into(holder.imageView);
 
             string = jsonObject.get("title") + "";
-            ((TextView) view.findViewById(R.id.titleTextView)).setText(string);
+            holder.textView.setText(string);
 
             string = jsonObject.get("content") + "";
             string = string.replace("\"","");
@@ -79,22 +80,34 @@ public class NewsDetailActivity extends AppCompatActivity {
                 string = string.substring(0, string.lastIndexOf("["));
             else
                 string = jsonObject.get("description")+"";
-            ((TextView) view.findViewById(R.id.bodyTextView)).setText(string);
+            holder.tV.setText(string);
 
             jsonObject = (JsonObject) jsonObject.get("source");
             string = jsonObject.get("name") + "";
             if (!string.equalsIgnoreCase("null"))
-                ((TextView) view.findViewById(R.id.extraTextView)).setText("swipe left for more at " + string);
+                holder.t.setText("swipe left for more at " + string);
 
-            activityNewsDetailBinding.verticalViewPager.addView(view);
-
-            return view;
         }
 
         @Override
-        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-            //super.destroyItem(container, position, object);
-            container.removeView((View) object);
+        public int getItemCount() {
+            return jsonArray.size();
+        }
+
+        private class InnerClass extends RecyclerView.ViewHolder
+        {
+            private TextView textView,tV,t;
+            private ImageView imageView;
+
+            private InnerClass(@NonNull View itemView)
+            {
+                super(itemView);
+
+                textView = itemView.findViewById(R.id.titleTextView);
+                tV = itemView.findViewById(R.id.bodyTextView);
+                t = itemView.findViewById(R.id.extraTextView);
+                imageView = itemView.findViewById(R.id.imageView);
+            }
         }
     }
 }
